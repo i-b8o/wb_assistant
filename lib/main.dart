0d3c6d8b3d54f344ff6e48d64261a12d81faa800
bogach-ivan <blackview1617@gmail.com>
2022-06-22 17:17:40 +0300

    fsdfdsfsd

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'simple_bloc_observer.dart';
 import 'views/app/app.dart';
 
+// TODO Automatically count returns average price (65 + 33) in dependence of income number
 Future<void> main() async {
   BlocOverrides.runZoned(
     () => runApp(App()),
