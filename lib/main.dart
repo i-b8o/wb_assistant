449cbf436aa832229ccda84bbd5c8ae00e0a8ce6
bogach-ivan <blackview1617@gmail.com>
2022-06-22 17:27:35 +0300

    fsdfdsfsd

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'simple_bloc_observer.dart';
 import 'views/app/app.dart';
 
 // TODO Automatically count returns average price (65 + 33) in dependence of income number
+// TODO After signIn pop signPage
 Future<void> main() async {
   BlocOverrides.runZoned(
     () => runApp(App()),
